11a59995efa5db371677ba5a8aa5d595d5a13619
ederleyR <ederley123@gmail.com>
2026-05-14 15:08:40 -0500

    Update excelController.php

--- controllers/excelController.php
+++ controllers/excelController.php
@@ -10,14 +10,12 @@
             $user_id = $_SESSION['user_id'];
             $excelModel = new EXCEL();
             
-            // Llamamos una sola vez y obtenemos el paquete de datos
+            
             $datosReporte = $excelModel->getReservasExcel($user_id); 
 
-            // Separamos los datos para que estén listos para 'report/excel.php'
             $usuario  = $datosReporte['usuario'];
             $reservas = $datosReporte['reservas']; 
 
-            // Validamos que existan reservas antes de cargar la vista
             if (empty($reservas)) {
                 $_SESSION['errores'] = ['general' => 'No tienes reservas para exportar.'];
                 header('Location: index.php?action=getMisReservas');
